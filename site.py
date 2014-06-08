72f6e44a623ab4c72e96ef6f6d879f546a27669c
Gianni Moschini <gianni.moschini@hailocab.com>
2014-06-08 12:20:32 +0100

    Tail tmp file when connection is open

--- site.py
+++ site.py
@@ -8,7 +8,11 @@ import pytz
 import pymongo
 from bson.objectid import ObjectId
 import os
+import tail
+import time
+from watcher import LogWatcher
 from sockjs.tornado import SockJSRouter, SockJSConnection
+from sockjs.tornado.periodic import Callback
 from bson import json_util
 
 class MySocket(SockJSConnection):
@@ -26,6 +30,7 @@ class EchoConnection(MySocket):
         collection.find(query, tailable=True, await_data=True).tail(callback=self.on_new_requests)
         
     def on_message(self, msg):
+        
         print "message ", msg
 
     def on_new_requests(self, result, err):
@@ -37,6 +42,63 @@ class EchoConnection(MySocket):
 
             self.send(msg)
 
+class BodyConnection(SockJSConnection):
+    filet = None
+    position = 0
+
+    def on_open(self, info):
+        print "open"
+        
+    def on_message(self, fileid):
+        print "message [" + fileid + "]"
+        filepath = "/Users/giannimoschini/src/github.com/leibowitz/go-proxy-service/" + fileid
+        self.tail_file(filepath)
+        
+    @gen.engine
+    def tail_file(self, filepath):
+        print "opening file"
+        #self.position = 0
+        self._file = open(filepath)
+        self.position = 0
+
+        # Go to the end of file
+        self._file.seek(0,2)
+        self.position = self._file.tell()
+
+        self.pcb = tornado.ioloop.PeriodicCallback(self.check_data, 1000)
+        self.pcb.start()
+
+        # check for new data every second        
+        #self.pcb = tornado.ioloop.PeriodicCallback(self.check_data, 1000)
+        #self.pcb = Callback(self.check_data)
+        #self.pcb.start()
+        #print "started"
+
+    def check_data(self):
+        print "looking at data"
+        if not self._file.closed:
+            self._file.seek(self.position)
+            line = self._file.readline()
+            if line:
+                self.position=self._file.tell()
+                self.on_new_data(line)
+
+#        self.on_new_data(data)
+        #self.position = self.filet.follow(blocking=False, position=self.position)
+
+    def on_new_data(self, data):
+        print "data ", data
+        self.send(data)
+
+    def on_close(self):
+        print "closing"
+        if self._file is not None and not self._file.closed:
+            self._file.close()
+        self.pcb.stop()
+        #self.position = 0
+        #self.pcb.stop()
+        #if self.filet != None:
+        #    print self.filet
 
 
 
@@ -70,11 +132,16 @@ class ViewHandler(tornado.web.RequestHandler):
         entry = yield motor.Op(collection.find_one, {'_id': ObjectId(ident)})
         requestheaders = nice_headers(entry['request']['headers'])
         responseheaders = nice_headers(entry['response']['headers'])
+        print entry['request']
 
-        requestbody = nice_body(entry['request']['body'], requestheaders)
-        responsebody = nice_body(entry['response']['body'], responseheaders)
+        #requestbody = nice_body(entry['request']['body'], requestheaders)
+        #responsebody = nice_body(entry['response']['body'], responseheaders)
 
-        self.render("one.html", item=entry, requestheaders=requestheaders, responseheaders=responseheaders, requestbody=requestbody, responsebody=responsebody)
+        self.render("one.html", item=entry, 
+                requestheaders=requestheaders, 
+                responseheaders=responseheaders)
+                #requestbody=requestbody, 
+                #responsebody=responsebody)
 
 db = motor.MotorClient('mongodb://localhost:17017', tz_aware=True).open_sync().proxyservice
 EST = pytz.timezone('Europe/London')
@@ -94,6 +161,9 @@ settings = dict(
 
 EchoRouter = SockJSRouter(EchoConnection, '/echo')
 handlers.extend(EchoRouter.urls)
+
+BodyRouter = SockJSRouter(BodyConnection, '/body')
+handlers.extend(BodyRouter.urls)
 
 application = tornado.web.Application(
     **settings)
