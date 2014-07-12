42bc46049a957c8e792282765684429448a202e4
Gianni Moschini <gianni.moschini@hailocab.com>
2014-07-12 23:25:05 +0100

    Show user-agent next to client IP

--- site.py
+++ site.py
@@ -26,6 +26,17 @@ from pygments.lexers import JsonLexer, TextLexer, IniLexer, get_lexer_for_mimety
 from pygments.formatters import HtmlFormatter
 import tempfile
 from multiplex import MultiplexConnection
+import re
+
+def find_agent(useragent):
+    ismatch = re.search("\((?P<agent>[^\)]+)\)", useragent)
+    if ismatch:
+        parts = ismatch.groupdict()['agent'].split('; ')
+        if len(parts) == 1:
+            return parts[0]
+        elif len(parts) > 1:
+            return parts[1]
+    return useragent
 
 def cleanarg(arg, default=None):
     if arg is None:
@@ -52,7 +63,11 @@ def get_uuid(entry):
     return str(uuid.UUID(bytes=entry['uuid'])) if 'uuid' in entry else None
 
 def get_content_type(headers):
-    return headers['Content-Type'] if 'Content-Type' in headers else None
+    return get_header(headers, 'Content-Type')
+
+def get_header(headers, key, default=None):
+    return headers[key] if key in headers else default
+
 
 # http://www.iana.org/assignments/media-types/media-types.xhtml
 # http://en.wikipedia.org/wiki/Internet_media_type#Type_text
@@ -275,10 +290,40 @@ class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     @gen.engine
     def get(self):
+        # Get Latest 20 hosts 
         collection = self.settings['db'].proxyservice['log_logentry']
-        d = datetime.utcnow() - timedelta(hours=1)
-        entries = yield collection.find({"date": {"$gte":d}}).sort([("$natural", pymongo.DESCENDING)]).limit(200).distinct("request.origin")
-        self.render("index.html", items=filter(None, entries), host=self.request.host, port=self.settings['proxyport'], ip=self.settings['proxyhost'] or self.request.host.split(':')[0], yourip=self.request.remote_ip)
+
+        # for the last 24 hours
+        d = datetime.utcnow() - timedelta(hours=100)
+
+        # 
+        hosts = yield collection.find({"date": {"$gte":d}}).\
+            sort([("$natural", pymongo.DESCENDING)]).\
+            limit(200).\
+            distinct("request.origin")
+
+        # filter empty hosts - not necessary but could
+        # happen if origin was not set on one request
+        hosts = filter(None, hosts)
+
+        # Get one request from each host to try
+        # to sniff the user-agent
+        entries = {}
+        for host in hosts:
+            request = yield collection.find_one({'request.origin':host}, {"request.headers":1})
+            entries[host] = find_agent(get_header(nice_headers(request['request']['headers']), 'User-Agent'))
+
+        # if proxyhost was set when starting, use it
+        # otherwise determine host/ip by looking at the Host request header
+        proxyip = self.settings['proxyhost'] or self.request.host.split(':')[0]
+        proxyport = self.settings['proxyport']
+
+        self.render("index.html", 
+                items=entries, 
+                host=self.request.host, 
+                port=proxyport, 
+                ip=proxyip, 
+                yourip=self.request.remote_ip)
 
 class OriginHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
