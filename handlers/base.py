1dab0482caea613b72237c34776c383d6849a95f
Gianni Moschini <gianni.moschini@hailocab.com>
2016-02-27 11:59:31 +0000

    Fix for get_submitted_headers

--- handlers/base.py
+++ handlers/base.py
@@ -26,8 +26,8 @@ class BaseRequestHandler(RequestHandler):
             if len(row[0].strip()) != 0:
                 headers[ row[0] ] = row[1]
 
-            row = self.get_arguments(fieldname+'[' + str(x) + '][]', [])
             x += 1
+            row = self.get_arguments(fieldname+'[' + str(x) + '][]', [])
 
         return headers
 
