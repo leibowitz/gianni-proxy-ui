ef8cf4d86613a7cfd64a86195b4ce3ea9a6ffb9f
Gianni Moschini <gianni.moschini@hailocab.com>
2016-03-12 22:48:21 +0000

    Merge sessions and origins pages

--- handlers/origins.py
+++ handlers/origins.py
@@ -1,20 +1,54 @@
 from tornado import gen
 import tornado.web
+from datetime import datetime
+from datetime import timedelta
+import pymongo
+
+from shared import util
 
 from base import BaseRequestHandler
 
 class OriginsHandler(BaseRequestHandler):
-    def get(self):
-        self.show_list()
-
     @tornado.web.asynchronous
     @gen.engine
-    def show_list(self):
+    def get(self):
         collection = self.settings['db'].proxyservice['origins']
         cursor = collection.find({}).sort([('name', 1)])
         res = cursor.to_list(100)
-        entries = yield res
-        self.render("origins.html", items=entries)
+        results = yield res
+        #for entry in results:
+        #    print entry
+
+        # Get Latest 20 hosts 
+        collection = self.settings['db'].proxyservice['log_logentry']
+
+        # for the last 100 hours
+        d = datetime.utcnow() - timedelta(hours=100)
+
+        # only check for the last 200 items
+        hosts = yield collection.find({"date": {"$gte":d}}).\
+            sort([("$natural", pymongo.DESCENDING)]).\
+            limit(200).\
+            distinct("request.origin")
+        
+        # filter empty hosts - not necessary but could
+        # happen if origin was not set on one request
+        hosts = filter(None, hosts)
+
+        # make a dictionary out of results list
+        rkeys = dict((r["origin"], r) for r in results)
+
+        # Get one request from each host to try
+        # to sniff the user-agent
+        for host in hosts:
+            request = yield collection.find_one({'request.origin':host}, {"request.headers":1})
+            agent = util.find_agent(util.get_header(self.nice_headers(request['request']['headers']), 'User-Agent'))
+            if host not in rkeys:
+                rkeys[host] = {"origin": host, "agent": agent}
+            else:
+                rkeys[host]['agent'] = agent
+        
+        self.render("origins.html", items=rkeys)
 
     def post(self):
         collection = self.settings['db'].proxyservice['origins']
@@ -29,6 +63,6 @@ class OriginsHandler(BaseRequestHandler):
 
         if 'X-Requested-With' in self.request.headers and self.request.headers['X-Requested-With'] == "XMLHttpRequest":
             return
-
-        self.show_list()
+        
+        self.redirect("/origins")
 
