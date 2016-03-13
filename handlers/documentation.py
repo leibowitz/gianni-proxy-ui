8c583f6d3b9606ca154b1eb1d11301fba41ba3b8
Gianni Moschini <gianni.moschini@hailocab.com>
2016-03-13 08:51:34 +0000

    Add delete button for documents

--- handlers/documentation.py
+++ handlers/documentation.py
@@ -14,3 +14,15 @@ class DocumentationHandler(BaseRequestHandler):
         entries = yield res
         self.render("documentation.html", items=entries)
 
+    def post(self):
+        collection = self.settings['db'].proxyservice['documentation']
+        ident = self.get_argument('ident', None)
+        action = self.get_argument('action', None)
+        if action == "delete":
+            collection.remove({'_id': self.get_id(ident)})
+
+        if 'X-Requested-With' in self.request.headers and self.request.headers['X-Requested-With'] == "XMLHttpRequest":
+            return
+
+        self.redirect('/documentation')
+
