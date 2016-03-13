764e02bbd2d1109507ccc91b8d56c72b3902e730
Gianni Moschini <gianni.moschini@hailocab.com>
2016-03-13 23:45:47 +0000

    Re-use get_gridfs_body on doc view page

--- handlers/documentationview.py
+++ handlers/documentationview.py
@@ -21,8 +21,7 @@ class DocumentationViewHandler(BaseRequestHandler):
         origin = self.get_argument('origin', None)
         host = self.get_argument('host', None)
         collection = self.settings['db'].proxyservice['documentation']
-        body = None
-        fs = motor.MotorGridFS(self.settings['db'].proxyservice)
+        resbody = None
 
         try:
             oid = objectid.ObjectId(ident)
@@ -43,105 +42,32 @@ class DocumentationViewHandler(BaseRequestHandler):
         requestbody = None
         responsebody = None
 
-        socketuuid = util.get_uuid(entry)
-
-        # consider the response finished
-        finished = True
-
-        #print entry['request']
-        #print entry['response']
         if 'fileid' in entry['response']:
-
-            respfileid = entry['response']['fileid']
-            filepath = os.path.join(tempfile.gettempdir(), "proxy-service", str(respfileid))
-            #print filepath
+            resbody, ctype = yield self.get_gridfs_body(entry['response']['fileid'], responseheaders)
+            responsebody = self.get_formatted_body(resbody, ctype)
             
-            response_mime_type = util.get_content_type(requests.structures.CaseInsensitiveDict(responseheaders))
-            if not os.path.exists(filepath):
-                body = yield util.get_gridfs_content(fs, respfileid)
-                if body:
-                    if not response_mime_type:
-                        pass
-                    elif 'text/plain' in response_mime_type:
-                        responsebody = util.get_body_non_empty_lines(body.strip().split("\n"))
-                    elif self.is_text_content(response_mime_type):
-                        responsebody = util.nice_body(body, response_mime_type)
-                    elif 'image' in response_mime_type:
-                        responsebody = util.raw_image_html(body, response_mime_type)
-            else:
-                if not response_mime_type:
-                    pass
-                elif 'text/plain' in response_mime_type:
-                    lines = open(filepath).readlines()
-                    responsebody = util.get_body_non_empty_lines(lines)
-                elif self.is_text_content(response_mime_type):
-                    content = open(filepath).read()
-                    responsebody = util.nice_body(content, response_mime_type)
-                elif 'image' in response_mime_type:
-                    responsebody = util.raw_image_html(content, response_mime_type)
-                # request seems to be still open
-                finished = False
-                #responsebody = open(filepath).read()
-                #ctype = responseheaders['Content-Type']
-                #responsebody = util.nice_body(responsebody, ctype)
+        if 'fileid' in entry['request']:
+            reqbody, ctype = yield self.get_gridfs_body(entry['request']['fileid'], requestheaders)
+            requestbody = self.get_formatted_body(reqbody, ctype)
 
-        cmd = 'curl' 
-        cmd = cmd + ' -X ' + entry['request']['method']
         for key, value in requestheaders.iteritems():
-            cmd = cmd + ' -H ' + util.QuoteForPOSIX(key + ': ' + value)
             if key == 'Cookie':
                 requestheaders[key] = util.nice_body(value, 'application/x-www-form-urlencoded')
 
-        if 'fileid' in entry['request'] and not self.has_binary_content(requestheaders):
-            requestbody = yield util.get_gridfs_content(fs, entry['request']['fileid'])
-            if requestbody:
-                ctype = util.get_content_type(requestheaders)
-                # default to x-www-form-urlencoded
-                ctype = ctype if ctype is not None else 'application/x-www-form-urlencoded'
-                #if self.has_text_content(requestheaders)
-
-                if util.get_content_encoding(requestheaders) == 'gzip':
-                    requestbody = util.ungzip(requestbody)
-
-                    ctype = None
-
-                bodyparam = util.QuoteForPOSIX(requestbody)
-                try:
-                    cmd = cmd + ' -d ' + bodyparam
-                except Exception as e:
-                    print e
-
-                requestbody = util.nice_body(requestbody, ctype)
-        #requestbody = util.nice_body(entry['request']['body'], requestheaders)
-        #responsebody = util.nice_body(entry['response']['body'], responseheaders)
-
-        cmd = cmd + ' ' + util.QuoteForPOSIX(entry['request']['url'])
-
-        if entry['request']['method'] == 'GET':
-            collection = self.settings['db'].proxyservice['log_messages']
-            messages = yield collection.find({"host": entry['request']['host']}).sort('_id').to_list(100)
-            for msg in messages:
-                msg['message'] = re.escape(msg['message'])
-        else:
-            messages = {}
-
-        # colorise bash command
-        cmd = highlight(cmd, BashLexer(), HtmlFormatter(cssclass='codehilite curl'))
         fmt = util.get_format(util.get_content_type(self.nice_headers(responseheaders))) if responseheaders else None
 
         self.render("one.html", 
                 item=entry, 
-                body=body,
+                body=resbody,
                 fmt=fmt,
-                cmd=cmd,
-                messages=messages,
+                cmd=None,
                 requestheaders=requestheaders, 
                 responseheaders=responseheaders,
                 requestbody=requestbody, 
                 responsebody=responsebody,
                 requestquery=requestquery, 
-                finished=finished,
-                socketuuid=socketuuid,
+                finished=True,
+                socketuuid=None,
                 origin=origin,
                 host=host,
                 show_save=False,
