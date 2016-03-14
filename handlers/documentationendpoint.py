38fc5568229e977769f1d9415e4b4fabb3c70779
Gianni Moschini <gianni.moschini@hailocab.com>
2016-03-14 10:26:30 +0000

    Pass list of examples to doc page

--- handlers/documentationendpoint.py
+++ handlers/documentationendpoint.py
@@ -20,12 +20,12 @@ class DocumentationEndpointHandler(BaseRequestHandler):
         collection = self.settings['db'].proxyservice['documentation']
         cursor = collection.find({'request.host': host}).sort([('request.path', 1)])
         res = cursor.to_list(100)
-        entries = yield res
+        endpoints = yield res
 
         # Get a list of requests per endpoints
         tree = util.tree()
 
-        for item in entries:
+        for item in endpoints:
             parts = filter(None, item['request']['path'].split('/'))
             o = tree
             for part in parts:
@@ -33,7 +33,7 @@ class DocumentationEndpointHandler(BaseRequestHandler):
 
             o['methods'][item['request']['method']][item['response']['status']] = item['_id']
 
-        entry = yield collection.find_one({'request.host': host, 'request.path': path, 'request.method': method})
+        entries = yield collection.find({'request.host': host, 'request.path': path, 'request.method': method}).to_list(100)
 
         requestheaders = {}
         responseheaders = {}
@@ -43,7 +43,9 @@ class DocumentationEndpointHandler(BaseRequestHandler):
         restype = None
         reqfields = {}
 
-        if entry:
+        entry = None
+        if len(entries) != 0:
+            entry = entries[0]
             requestheaders = self.nice_headers(entry['request']['headers'])
             responseheaders = self.nice_headers(entry['response']['headers'])
 
@@ -55,20 +57,19 @@ class DocumentationEndpointHandler(BaseRequestHandler):
             if 'response' in entry and 'fileid' in entry['response']:
                 resbody, restype = yield self.get_gridfs_body(entry['response']['fileid'], responseheaders)
 
-        self.render("documentationhost.html", host=host, entry=entry, tree=tree, render_tree=self.render_tree, render_document=self.render_document, requestheaders=requestheaders, responseheaders=responseheaders, reqbody=reqbody, resbody=resbody, reqtype=reqtype, restype=restype, reqfields=reqfields, currentpath=path)
+        self.render("documentationhost.html", host=host, entries=entries, entry=entry, tree=tree, render_tree=self.render_tree, render_document=self.render_document, requestheaders=requestheaders, responseheaders=responseheaders, reqbody=reqbody, resbody=resbody, reqtype=reqtype, restype=restype, reqfields=reqfields, currentpath=path)
 
     def render_tree(self, host, tree, currentpath=None, fullpath = ''):
         return self.render_string("documentationtree.html", host=host, tree=tree, render_tree=self.render_tree, fullpath=fullpath+'/', currentpath=currentpath)
 
-    def render_document(self, entry, requestheaders, responseheaders, reqbody, resbody, reqtype, restype, reqfields):
+    def render_document(self, entry, entries, requestheaders, responseheaders, reqbody, resbody, reqtype, restype, reqfields):
         schema = None
         if util.get_format(restype) == 'json':
             schema = skinfer.generate_schema(json.loads(resbody))
-            print schema
             #print genson.Schema().add_object(json.loads(resbody)).to_dict()
         
         query = urlparse.parse_qsl(entry['request']['query'], keep_blank_values=True)
-        return self.render_string("documentationendpoint.html", entry=entry, requestheaders=requestheaders, responseheaders=responseheaders, resbody=resbody, reqbody=reqbody, schema=schema, query=query, render_schema=self.render_schema, reqfields=reqfields)
+        return self.render_string("documentationendpoint.html", entry=entry, entries=entries, requestheaders=requestheaders, responseheaders=responseheaders, resbody=resbody, reqbody=reqbody, schema=schema, query=query, render_schema=self.render_schema, reqfields=reqfields)
 
     def render_schema(self, schema):
         return self.render_string("documentationschema.html", schema=schema, render_schema=self.render_schema)
