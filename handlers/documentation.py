6dbf856719121588653b9049bb50e0ca49627c82
Gianni Moschini <gianni.moschini@hailocab.com>
2016-08-07 15:30:04 +0100

    Add ability to filter list page by host

--- handlers/documentation.py
+++ handlers/documentation.py
@@ -9,7 +9,11 @@ class DocumentationHandler(BaseRequestHandler):
     @gen.engine
     def get(self):
         collection = self.settings['db'].proxyservice['documentation']
-        cursor = collection.find({}).sort([('request.host', 1), ('request.path', 1)])
+        host = self.get_argument('host', None)
+        req = {}
+        if host:
+            req['request.host'] = host
+        cursor = collection.find(req).sort([('request.host', 1), ('request.path', 1)])
         res = cursor.to_list(100)
         entries = yield res
         self.render("documentation.html", items=entries)
