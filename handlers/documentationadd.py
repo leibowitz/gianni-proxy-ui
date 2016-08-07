76788dd879103b214fd41d7d096ed772fe6874b5
Gianni Moschini <gianni.moschini@hailocab.com>
2016-08-07 15:28:13 +0100

    Redirect to documentation host page after add

--- handlers/documentationadd.py
+++ handlers/documentationadd.py
@@ -73,5 +73,5 @@ class DocumentationAddHandler(BaseRequestHandler):
 
         yield collection.insert(item)
 
-        self.redirect('/documentation')
+        self.redirect('/documentation/host/' + host)
 
